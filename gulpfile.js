f62c7ee48de2fcc232a5ba7cfddb7fc6c6ce8150
elaine <kim85326@gmail.com>
2018-10-08 00:56:28 +0800

    首頁完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const minifyCSS = require('gulp-minify-css');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const rename = require('gulp-rename');
 
 gulp.task('sass', function() { // 定義 sass 的任務名稱
     return gulp.src('./src/scss/**/*.scss') // sass 的來源資料夾
@@ -10,12 +15,35 @@ gulp.task('sass', function() { // 定義 sass 的任務名稱
         .pipe(autoprefixer({
             browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
         }))
-        .pipe(gulp.dest('./public/stylesheets')); // sass 編譯完成後的匯出資料夾
+        .pipe(gulp.dest('./public/css')) // sass 編譯完成後的匯出資料夾
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifyCSS({
+            keepBreaks: true,
+        }))
+        .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('sass:watch', function() {
+var config = {
+    jsConcatFiles: [
+        './node_modules/jquery/dist/jquery.slim.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+    ]
+};
+
+gulp.task('js', function() {
+    return gulp.src(config.jsConcatFiles)
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./public/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('watch', function() {
     gulp.watch('./src/scss/**/*.scss', ['sass']);
-    // 監控資料夾，當有變化時執行 'sass' 任務
+    gulp.watch('./src/js/**/*.js', ['js']);
+    // 監控資料夾，當有變化時執行 'watch' 任務
 });
 
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('default', ['sass', 'js', 'watch']);
